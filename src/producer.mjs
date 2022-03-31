5f38dcf47c43d0ae3f79501f75c48cbb5aa3fe83
Rupesh Tiwari <roopkt@gmail.com>
2022-03-31 11:36:32 -0400

    new app

--- src/producer.mjs
+++ src/producer.mjs
@@ -1,5 +1,5 @@
 import { Kafka } from 'kafkajs';
- 
+
 const kafka = new Kafka({
   clientId: 'my-app',
   brokers: [`localhost:9092`],
@@ -7,15 +7,23 @@ const kafka = new Kafka({
 
 const producer = kafka.producer();
 await producer.connect();
-await producer.send({
-    topic: 'test-topic',
-    messages: [
-        {
-            value: 'Hello KafkaJS user!',
-            key: '1'
-        }
-    ]
-}); 
+await producer.sendBatch({
+  topic: 'test-topic',
+  messages: createMessage(),
+});
+
+function createMessage() {
+  const messages = [];
+  for (let key = 0; key < 100; key++) {
+    const message = `Hello KafkaJS user! ${key}`;
+    messages.push({
+      key,
+      message,
+    });
+  }
+
+  return messages;
+}
 
 await producer.disconnect();
 console.log(`message produced`);
